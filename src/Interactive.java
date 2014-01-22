b2e90203ebe191f5b07e1d0811da41e66538a7ca
14smithl <14smithl@abschools.org>
2014-01-22 11:40:50 -0500

    Create Interactive class

---
+++ src/Interactive.java
@@ -0,0 +1,17 @@
+import javax.swing.JComponent;
+
+
+public class Interactive {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+class ShapeDrawingComponent extends JComponent{
+	private static final long serialVersionUID = 1L;
+	private Shape shape;
+	
+}
