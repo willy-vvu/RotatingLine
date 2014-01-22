f7a4d481173cc52a5b9180cbf780fb0a9a305db0
willy-vvu <willy.vvu@gmail.com>
2014-01-22 11:02:57 -0500

    Create empty Vector2 class

---
+++ src/Vector2.java
@@ -0,0 +1,13 @@
+/**
+ * Vector2.java
+ * 
+ * A class to create, manipulate, and rotate 2 dimensional vectors.
+ * 
+ * Written Jan 22, 2014.
+ * 
+ * @author William Wu
+ *
+ */
+public class Vector2 {
+
+}
